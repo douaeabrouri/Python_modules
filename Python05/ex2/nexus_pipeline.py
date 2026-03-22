03b70f221f873c4807eb531ac7c5ea430cf6ffac
douaeabrouri <abrouridoae@gmail.com>
2026-03-22 20:49:06 +0100

    ...

--- Python05/ex2/nexus_pipeline.py
+++ Python05/ex2/nexus_pipeline.py
@@ -12,6 +12,7 @@
 
 from abc import ABC, abstractmethod
 from typing import Protocol, Any
+import json
 
 class ProcessingStage(Protocol):
     def process(self, data: Any) -> Any:
@@ -30,8 +31,52 @@ class ProcessingPipeline(ABC):
         pass
 
 class InputStage():
-    pass
+    def process(self, data: Any) -> Any:
+        return f"Input: {data}"
 class TransformStage():
-    pass
+    def process(self, data: Any) -> Any:
+        return f"Transfor: {str(data).upper()}"
+
 class OutputStage():
-    pass
+    def process(self, data: Any) -> Any:
+        return f"Output: {data}"
+
+class JSONAdapter(ProcessingPipeline):
+    def process(self, data: Any) -> Any:
+        print("Processing JSON data through pipeline...")
+        print(f"Input: {data}")
+        result: Any = data
+        for stage in self.stages:
+            result = stage.process(result)
+        return result
+
+class CSVAdapter(ProcessingPipeline):
+    def process(self, data: Any) -> Any:
+        print("Processing CSV data through pipeline...")
+        print(f"Input: {data}")
+        result: Any = data
+        for stage in self.stages:
+            result = stage.process(result)
+        return result
+
+class StreamAdapter(ProcessingPipeline):
+    def process(self, data: Any) -> Any:
+        print("Processing Stream data through pipeline...")
+        print(f"Input: {data}")
+        result: Any = data
+        for stage in self.stages:
+            result = stage.process(result)
+        return result
+
+class NexusManager:
+    
+
+if __name__ == "__main__":
+    data = {"val": 24, "val2": 25}
+    # test = TransformStage.process(data)
+    json_pipeline = CSVAdapter("JSON_001")
+    json_pipeline.add_stages(InputStage())
+    json_pipeline.add_stages(TransformStage())
+    json_pipeline.add_stages(OutputStage())
+    resul = json_pipeline.process(data)
+    print(resul)
